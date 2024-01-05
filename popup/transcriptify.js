ddfc0e7379eb3a0e866868d6ba917a9cb7a02458
Mo Aziz <mohammed4685@outlook.com>
2024-01-05 22:42:38 +0000

    Updated the JS file for functions - may refactor later. Will add logic later.

--- popup/transcriptify.js
+++ popup/transcriptify.js
@@ -1,3 +1,30 @@
-()=>{
+const setHandlers = ()=>{
+    document.addEventListener('click', (e)=>{
+        function onDownload(){
+
+        }
+
+        function onInput(){
+
+        }
+
+        function onLoad(){
+
+        }
+
+        function onSearch(){
+            
+        }
+
+    })
 
 }
+
+const reportError = ()=>{
+
+};
+
+browser.tabs
+    .executeScript()
+    .then(setHandlers)
+    .catch(reportError);
