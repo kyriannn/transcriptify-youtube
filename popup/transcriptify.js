594987bfce67ba4a86a535bdec18afa833026a79
Mo Aziz <mohammed4685@outlook.com>
2024-01-06 14:00:00 +0000

    added comments in preparation for logic writing

--- popup/transcriptify.js
+++ popup/transcriptify.js
@@ -1,19 +1,41 @@
 const setHandlers = ()=>{
     document.addEventListener('click', (e)=>{
         function onDownload(){
+            e.preventDefault();
+
+            //check if download is successful
+
+            //if not, return fail state
+
+            //else return success state
+
+
 
         }
 
         function onInput(){
+            //validate & sanitize input
+
+
+            //perhaps test if the input is of a legitimate file path? if possible, not sure
+
+
 
         }
 
         function onLoad(){
+            //check if current link is of youtube
 
         }
 
         function onSearch(){
+            //check if captions are present for the requested youtube vid
+            //if not, error
             
+
+
+            //else change state to download and give a file location
+
         }
 
     })
