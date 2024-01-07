b4a56695089fdb9503d8b0169ac2ce8e7a3c1515
Mo Aziz <mohammed4685@outlook.com>
2024-01-07 21:19:08 +0000

    Quick refactor for the JS logic, and name change in the HTML to allow for use in JS.

--- popup/transcriptify.js
+++ popup/transcriptify.js
@@ -1,48 +1,39 @@
 const setHandlers = ()=>{
-    document.addEventListener('click', (e)=>{
-        function onDownload(){
-            e.preventDefault();
+    
 
-            //check if download is successful
+    document.body.addEventListener('load', ()=>{
+        //check if current link is of youtube
 
-            //if not, return fail state
-
-            //else return success state
-
-
-
-        }
-
-        function onInput(){
-            //validate & sanitize input
-
-
-            //perhaps test if the input is of a legitimate file path? if possible, not sure
-
-
-
-        }
-
-        function onLoad(){
-            //check if current link is of youtube
-
-        }
-
-        function onSearch(){
-            //check if captions are present for the requested youtube vid
-            //if not, error
-            
-
-
-            //else change state to download and give a file location
-
-        }
+        //else return error
 
     })
 
+    find_captions.addEventListener('click', ()=>{
+        
+
+    })
+
+    download_captions.addEventListener('click', ()=>{
+        //check if download is successful
+
+        //if not, return fail state
+
+        //else return success state
+    })
+    
+    download_location.addEventListener('input', ()=>{
+        //on input
+
+        //test if it is a valid file path
+
+        //if not, return error
+    })
+
+    
+
 }
 
-const reportError = ()=>{
+const reportError = (e)=>{
 
 };
 
